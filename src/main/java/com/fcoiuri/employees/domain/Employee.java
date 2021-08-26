8947f95dd2a57dec67f8a1c17846143a604048fc
fcoiuri <fcoiuri@gmail.com>
2021-08-26 12:04:14 -0300

    classe de dominio criada

---
+++ src/main/java/com/fcoiuri/employees/domain/Employee.java
@@ -0,0 +1,75 @@
+package com.fcoiuri.employees.domain;
+
+import java.util.Objects;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String jobTitle;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id.equals(employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String email, String phone, String jobTitle) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.jobTitle = jobTitle;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}

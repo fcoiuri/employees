38e79c14a16b8159df869d48e8a55049e71811d2
fcoiuri <fcoiuri@gmail.com>
2021-08-29 18:36:40 -0300

    atualizando service

--- src/main/java/com/example/employeemanager/service/EmployeeService.java
+++ src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.example.employeemanager.service;
 
 import com.example.employeemanager.domain.Employee;
+import com.example.employeemanager.exception.UserNotFoundException;
 import com.example.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,7 @@ public class EmployeeService {
 
     public Employee findEmployeeById(Long id){
         return employeeRepo.findEmployeeById(id).orElseThrow(()->
-                new UserNotFoundException ("Usuário do id: "+id+" não foi encontrado"));
+                new UserNotFoundException("Usuário do id: "+id+" não foi encontrado"));
     }
 
     public void deleteEmployee(Long id){
